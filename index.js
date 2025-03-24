2ad7340127e1825827a18e449706273894c4bd39
TxKaotic <daniels.stephen2011@yahoo.com>
2025-03-24 08:46:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+ textField = document.getElementById("generatedPassword")
+
+
+
+
+
+function generatePassword() {
+    
+    var length = 15,
+        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789" + "!. @#$",
+        retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+        retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+         
+        textField = retVal
+
+        console.log(textField)
+        alert(textField)
+        return retVal
+}        
+
+     
+
+
